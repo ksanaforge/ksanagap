bf44ae620132653e8c4b19a9a4a026b03a766e02
g1ahsia <g1ahsia@gmail.com>
2014-10-01 20:29:34 +0800

    init
    - fileread
    - filewrite
    - filereadbuffer

---
+++ ksanagap/ksanagapTests/ksanagapTests.m
@@ -0,0 +1,40 @@
+//
+//  ksanagapTests.m
+//  ksanagapTests
+//
+//  Created by Hsiao Allen on 10/1/14.
+//  Copyright (c) 2014 Hsiao Allen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ksanagapTests : XCTestCase
+
+@end
+
+@implementation ksanagapTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
---
+++ ksanagap/test.txt
@@ -0,0 +1,1 @@
+abdcefg
