bf667a00860b97b497950fa7b094cb187b99e26f
yap cheah shen <yapcheahshen@gmail.com>
2014-11-20 10:33:55 +0800

    add system menu item for visit store

--- android/app/src/main/java/ksanaforge/ksanagap/mainActivity.java
+++ android/app/src/main/java/ksanaforge/ksanagap/mainActivity.java
@@ -188,12 +188,20 @@ public class mainActivity extends Activity {
         ksanagap_api.switchApp(appname);
     }
 
+    protected void visitstore() {
+        startActivity(new Intent(Intent.ACTION_VIEW,
+                Uri.parse(this.getString(R.string.storeurl))));
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        if (id == R.id.visitstore) {
+            visitstore();
+            return false;
+        }
         gotoApp(id);
         return super.onOptionsItemSelected(item);
     }
